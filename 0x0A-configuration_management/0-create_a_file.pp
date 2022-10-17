ea1492fc2d89440f98b2fc6156023e98f198b150
Badmus328 <badmusridwa965>
2022-10-17 06:28:20 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,11 @@
-# Creates a file in /tmp
+# create a file in /tmp.
 
-file { 'school':
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+ file { '/tmp/':
+
+    ensure  => present,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
